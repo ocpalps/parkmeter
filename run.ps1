ed8c3fb4a6d126f056a1c65bed3a01810f1d72e6
Gianluca Bertelli <gianluca.bertelli@live.it>
2018-12-05 12:02:47 +0100

    powershell script to run also the functions

--- run.ps1
+++ run.ps1
@@ -1,4 +1,7 @@
-﻿#cosmos db
+﻿# BUILD
+Start-Process dotnet -ArgumentList "build .\src\ParkMeter.sln" -Wait
+
+#cosmos db
 $cosmos = Get-Process CosmosDB.Emulator -ErrorAction SilentlyContinue
 if (-Not $cosmos) {
     Start-Process -FilePath "C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe"
@@ -13,3 +16,10 @@ Set-Location "src\FrontEnd\Parkmeter.Admin"
 Start-Process -FilePath dotnet  -ArgumentList "run", "--project Parkmeter.Admin.csproj"
 Set-Location "..\..\..\"
 
+# FUNCTIONS
+# DOWNLOAD LATEST RUNTIME
+Start-Process npm -ArgumentList "install azure-functions-core-tools -g" -wait
+
+Set-Location "src\BackEnd\Parkmeter.Functions\bin\Debug\netcoreapp2.1"
+Start-Process func -ArgumentList "start"
+Set-Location "..\..\..\"
