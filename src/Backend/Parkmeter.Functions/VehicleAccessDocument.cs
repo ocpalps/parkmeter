6ae18cd6ffbc1189d20298799482c7360c391447
Marcello Marchetti <mamarche@outlook.it>
2019-01-15 15:59:32 +0100

    fix merge

---
+++ src/Backend/Parkmeter.Functions/VehicleAccessDocument.cs
@@ -0,0 +1,15 @@
+﻿using Parkmeter.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parkmeter.Functions
+{
+    public class VehicleAccessDocument
+    {
+        public VehicleAccess Access { get; set; }
+        public VehicleAccessDocument() { }
+
+        public string id { get; set; }
+    }
+}
