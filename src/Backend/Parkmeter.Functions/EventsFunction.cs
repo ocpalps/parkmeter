0b8de9c9728f9b25f7a03a60d3e49271c8260507
Gianluca Bertelli <gianluca.bertelli@live.it>
2019-05-27 11:20:30 +0200

    missing files

---
+++ src/Backend/Parkmeter.Functions/EventsFunction.cs
@@ -0,0 +1,123 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Parkmeter.Functions
+{
+    public static class EventsFunction
+    {
+        [FunctionName("ErrorEvent")]
+        public static void ErrorEvent([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        {
+            log.LogInformation("Error Event: " + eventGridEvent.Data.ToString());
+        }
+
+        [FunctionName("SuccededEvent")]
+        public static void SuccededEvent([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        {
+            log.LogInformation("Success Event: " + eventGridEvent.Data.ToString());
+        }
+
+        [FunctionName("FileUploaded")]
+        public static async Task FileUploaded([BlobTrigger("upload/{name}", Connection = "ImageStorage")]Stream myBlob, string name, Uri uri, ExecutionContext context, ILogger log)
+        {
+            log.LogInformation($"File uploaded: {name} \n Size: {myBlob.Length} Bytes");
+
+            var config = Tools.InitConfig(context);
+
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+               new ApiKeyServiceClientCredentials(config["ComputerVisionKey"]),
+               new System.Net.Http.DelegatingHandler[] { });
+
+            computerVision.Endpoint = config["ComputerVisionEndpoint"];
+
+            var result = await ExtractRemoteTextAsync(computerVision, uri.AbsoluteUri);
+
+            if (result.Status != TextOperationStatusCodes.Succeeded)
+            {
+                var failure = new EventGridHelper.ParkmeterEvent()
+                {
+                    Message = "No text found on image",
+                    Type = EventGridHelper.EventType.Error
+                };
+                EventGridHelper.SendEvent(failure);
+            }
+
+            bool found = false;
+            string pattern = @"^(([a-zA-Z]{2}\d{3}[a-zA-Z]{2})|(([a-zA-Z]{2}|roma)(\d{5}|\d{6})))$";
+            foreach (string text in result.RecognitionResult.Lines.Select(t => t.Text))
+            {
+                string plate = text.Trim().Replace(" ", "");
+                var matched = System.Text.RegularExpressions.Regex.Match(plate, pattern);
+                if (matched.Success)
+                {
+                    var succeded = new EventGridHelper.ParkmeterEvent() {
+                        Message = "License plate recognized",
+                        Type = EventGridHelper.EventType.Succeded,
+                        Data = plate };
+                    EventGridHelper.SendEvent(succeded);
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found)
+            {
+                var failure = new EventGridHelper.ParkmeterEvent()
+                {
+                    Message = "License plate format is not valid",
+                    Type = EventGridHelper.EventType.Error
+                };
+                EventGridHelper.SendEvent(failure);
+            }
+        }
+
+        // Recognize text from a remote image
+        private async static Task<TextOperationResult> ExtractRemoteTextAsync(ComputerVisionClient computerVision, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Debug.WriteLine("\nInvalid remoteImageUrl:\n{0} \n", imageUrl);
+                return null;
+            }
+
+            // Start the async process to recognize the text
+            RecognizeTextHeaders textHeaders = await computerVision.RecognizeTextAsync(imageUrl, TextRecognitionMode.Printed);
+
+            // Retrieve the URI where the recognized text will be
+            // stored from the Operation-Location header
+            string operationId = textHeaders.OperationLocation.Substring(textHeaders.OperationLocation.Length - 36);
+
+            TextOperationResult result = await computerVision.GetTextOperationResultAsync(operationId);
+
+            // Wait for the operation to complete
+            int i = 0;
+            int maxRetries = 10;
+            while ((result.Status == TextOperationStatusCodes.Running ||
+                    result.Status == TextOperationStatusCodes.NotStarted) && i++ < maxRetries)
+            {
+                Debug.WriteLine("Server status: {0}, waiting {1} seconds...", result.Status, i);
+                await Task.Delay(1000);
+
+                result = await computerVision.GetTextOperationResultAsync(operationId);
+            }
+
+            // Return the results
+            return result;
+        }
+
+    }
+}
