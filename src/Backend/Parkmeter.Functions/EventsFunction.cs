73a32b7e7d0d59e7502cc150f731fa5872dd957a
Marcello Marchetti <mamarche@outlook.it>
2019-05-28 10:09:52 +0200

    ServiceBus Integration

--- src/Backend/Parkmeter.Functions/EventsFunction.cs
+++ src/Backend/Parkmeter.Functions/EventsFunction.cs
@@ -25,40 +25,61 @@ namespace Parkmeter.Functions
     {
         [FunctionName("ErrorEvent")]
         public static async Task ErrorEvent([EventGridTrigger]EventGridEvent eventGridEvent,
-            [CosmosDB(ConnectionStringSetting = "CosmosDBEndpoint")] DocumentClient client, 
             ILogger log)
         {
-            var error = new
-            {
-                Message = eventGridEvent.Data.ToString()
-            };
-
-            await FunctionsHelper.SaveEntryAsync(error, "Errors", client, log);
-
+           
             log.LogInformation("Error Event: " + eventGridEvent.Data.ToString());
+
+            //TODO: do something useful
         }
 
-        [FunctionName("SuccededEvent")]
-        public static async Task SuccededEvent([EventGridTrigger]EventGridHelper.ParkmeterEvent eventGridEvent,
+        [FunctionName("TrafficWorker")]
+        public static async Task TrafficWorker([ServiceBusTrigger(queueName:"trafficqueue", Connection = "ServiceBusConnection")] VehicleAccess vehicleAccess,
             [CosmosDB(ConnectionStringSetting = "CosmosDBEndpoint")] DocumentClient client,
             ILogger log)
         {
-            var va = new VehicleAccessDocument() { 
-                Access = JsonConvert.DeserializeObject<VehicleAccess>(eventGridEvent.Data)
-            };
+            try
+            {
+                var va = new VehicleAccessDocument()
+                {
+                    Access = vehicleAccess
+                };
 
+                await FunctionsHelper.SaveEntryAsync(va, "VehicleAccesses", client, log);
+                log.LogInformation($"Entry saved succesfully: {vehicleAccess.VehicleID}");
 
-            await FunctionsHelper.SaveEntryAsync(va, "VehicleAccesses", client, log);
-            log.LogInformation("Success Event: " + eventGridEvent.Data.ToString());
+                var succeded = new EventGridHelper.ParkmeterEvent()
+                {
+                    Message = $"Vehicle: {vehicleAccess.VehicleID} - Direction: {vehicleAccess.Direction.ToString()}",
+                    Type = EventGridHelper.EventType.Succeded
+                };
+                EventGridHelper.SendEvent(succeded);
+                log.LogInformation($"Succeded event sent");
+
+            }
+            catch (Exception ex)
+            {
+                var failure = new EventGridHelper.ParkmeterEvent()
+                {
+                    Message = "Save to db failed",
+                    Type = EventGridHelper.EventType.Error
+                };
+                EventGridHelper.SendEvent(failure);
+                log.LogInformation($"Error event sent");
+            }
+
         }
 
         [FunctionName("FileUploaded")]
-        public static async Task FileUploaded([BlobTrigger("upload/{name}", Connection = "ImageStorage")]Stream myBlob, string name, Uri uri, ExecutionContext context, ILogger log)
+        public static async Task FileUploaded([BlobTrigger("upload/{name}", Connection = "ImageStorage")]Stream myBlob, string name, Uri uri, ExecutionContext context,
+            [ServiceBus("trafficqueue", Connection = "ServiceBusConnection", EntityType = EntityType.Queue)] IAsyncCollector<VehicleAccess> vehicleAccessQueue,
+            ILogger log)
         {
             log.LogInformation($"File uploaded: {name} \n Size: {myBlob.Length} Bytes");
 
             var config = Tools.InitConfig(context);
 
+            //send image for OCR recognition
             ComputerVisionClient computerVision = new ComputerVisionClient(
                new ApiKeyServiceClientCredentials(config["ComputerVisionKey"]),
                new System.Net.Http.DelegatingHandler[] { });
@@ -68,7 +89,7 @@ namespace Parkmeter.Functions
             var result = await ExtractRemoteTextAsync(computerVision, uri.AbsoluteUri);
 
             if (result.Status != TextOperationStatusCodes.Succeeded)
-            {
+            { 
                 var failure = new EventGridHelper.ParkmeterEvent()
                 {
                     Message = "No text found on image",
@@ -86,17 +107,19 @@ namespace Parkmeter.Functions
                 var matched = System.Text.RegularExpressions.Regex.Match(plate, pattern);
                 if (matched.Success)
                 {
-                    var succeded = new EventGridHelper.ParkmeterEvent() {
-                        Message = "License plate recognized",
-                        Type = EventGridHelper.EventType.Succeded,
-                        Data = JsonConvert.SerializeObject( new VehicleAccess() { Direction=1, ParkingID=1, SpaceID=2, VehicleID=plate, VehicleType= VehicleTypes.Car } );
+                    //Enqueue message to service bus queue
+                    vehicleAccessQueue.AddAsync(new VehicleAccess()
+                    {
+                        Direction = AccessDirections.In,
+                        ParkingID = 1,
+                        SpaceID = 2,
+                        VehicleID = plate,
+                        VehicleType = VehicleTypes.Car
+                    }));
 
-                    log.LogInformation(Message);
+                    log.LogInformation(succeded.Message);
 
-                    EventGridHelper.SendEvent(succeded);
                     found = true;
-
-                    log.LogInformation($"Succeded event sent");
 
                     break;
                 }
